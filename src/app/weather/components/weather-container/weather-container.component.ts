3a3e2a34db079d24785005162c5113979c2e3d09
matins <olorunsholamatins@gmail.com>
2023-07-11 10:34:51 +0100

    feat: commit

--- src/app/weather/components/weather-container/weather-container.component.ts
+++ src/app/weather/components/weather-container/weather-container.component.ts
@@ -4,6 +4,19 @@ import { GlobalMapComponent } from '../global-map/global-map.component';
 import { SunriseChartComponent } from '../sunrise-chart/sunrise-chart.component';
 import { UvChartComponent } from '../uv-chart/uv-chart.component';
 import { WindChartComponent } from '../wind-chart/wind-chart.component';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription, map, startWith } from 'rxjs';
+import { Profile, WeatherDetails } from 'src/app/model/weather';
+import { WeatherService } from 'src/app/services/weather.service';
+import {
+  AddProfile,
+  RemoveProfile,
+} from 'src/app/weather/store/actions/weather';
+import {
+  AppState,
+  selectAllProfile,
+  selectAllWeatherDetails,
+} from 'src/app/weather/store/reducers/weather';
 
 @Component({
   selector: 'app-weather-container',
@@ -18,4 +31,27 @@ import { WindChartComponent } from '../wind-chart/wind-chart.component';
   templateUrl: './weather-container.component.html',
   styleUrls: ['./weather-container.component.scss'],
 })
-export class WeatherContainerComponent {}
+export class WeatherContainerComponent {
+  stateProfile: Observable<Profile[]>;
+  stateWeatherDetails: Observable<WeatherDetails[]>;
+  clickEventSubscription?: Subscription;
+  data: any;
+
+  constructor(private shared: WeatherService, private store: Store<AppState>) {
+    this.stateProfile = store.select(selectAllProfile);
+    this.stateWeatherDetails = store.select(selectAllWeatherDetails);
+    this.clickEventSubscription = this.shared.getClickEvent().subscribe(() => {
+      this.getData();
+    });
+    this.getData();
+  }
+
+  getData() {
+    this.stateWeatherDetails?.forEach((e) => {
+      if (e.length > 0) {
+        this.data = e[0]?.data;
+        console.log(this.data);
+      }
+    });
+  }
+}
